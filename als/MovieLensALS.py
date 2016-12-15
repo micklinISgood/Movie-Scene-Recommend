a1c194888dd54b53bc7c1bb33198fd2384cb5a72
Mick Lin <cl3469@columbia.edu>
2016-12-14 19:51:48 -0500

    changed path

--- als/MovieLensALS.py
+++ als/MovieLensALS.py
@@ -11,14 +11,14 @@ def parseRating(line):
     """
     Parses a rating record in MovieLens format userId::movieId::rating::timestamp .
     """
-    fields = line.strip().split("::")
+    fields = line.strip().split(",")
     return long(fields[3]) % 10, (int(fields[0]), int(fields[1]), float(fields[2]))
 
 def parseMovie(line):
     """
     Parses a movie record in MovieLens format movieId::movieTitle .
     """
-    fields = line.strip().split("::")
+    fields = line.strip().split(",")
     return int(fields[0]), fields[1]
 
 def loadRatings(ratingsFile):
@@ -48,10 +48,10 @@ def computeRmse(model, data, n):
     return sqrt(predictionsAndRatings.map(lambda x: (x[0] - x[1]) ** 2).reduce(add) / float(n))
 
 if __name__ == "__main__":
-    if (len(sys.argv) != 3):
-        print "Usage: /path/to/spark/bin/spark-submit --driver-memory 2g " + \
-          "MovieLensALS.py movieLensDataDir personalRatingsFile"
-        sys.exit(1)
+    # if (len(sys.argv) != 3):
+    #     print "Usage: /path/to/spark/bin/spark-submit --driver-memory 2g " + \
+    #       "MovieLensALS.py movieLensDataDir personalRatingsFile"
+    #     sys.exit(1)
 
     # set up environment
     conf = SparkConf() \
@@ -60,18 +60,20 @@ if __name__ == "__main__":
     sc = SparkContext(conf=conf)
 
     # load personal ratings
-    myRatings = loadRatings(sys.argv[2])
+    myRatings = loadRatings("myRating.txt")
     myRatingsRDD = sc.parallelize(myRatings, 1)
     
     # load ratings and movie titles
 
-    movieLensHomeDir = sys.argv[1]
+    
 
+    rating_path = "s3://"
+    movie_path = "s3://"
     # ratings is an RDD of (last digit of timestamp, (userId, movieId, rating))
-    ratings = sc.textFile(join(movieLensHomeDir, "ratings.dat")).map(parseRating)
+    ratings = sc.textFile(rating_path).map(parseRating)
 
     # movies is an RDD of (movieId, movieTitle)
-    movies = dict(sc.textFile(join(movieLensHomeDir, "movies.dat")).map(parseMovie).collect())
+    movies = dict(sc.textFile(movie_path).map(parseMovie).collect())
 
     numRatings = ratings.count()
     numUsers = ratings.values().map(lambda r: r[0]).distinct().count()
