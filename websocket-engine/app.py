dcb31f90d0e2dba358ca22c4005da30bd6ef3372
Mick Lin <cl3469@columbia.edu>
2016-12-15 17:54:10 -0500

    send to specific client done

--- websocket-engine/app.py
+++ websocket-engine/app.py
@@ -1,15 +1,36 @@
 from flask import Flask, render_template,request
-from flask_socketio import SocketIO
-import boto.sns,json
+from flask_socketio import SocketIO,send,emit
+import boto.sns,json,inspect
 sns_conn = boto.sns.connect_to_region("us-east-1", profile_name='movie')
 # print sns_conn.aws_access_key_id
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+
+online = {}
+SidToUid = {}
+
 @socketio.on('connect')
 def remote():
-	print 'remote connected ip: %s'%request.remote_addr
+    print request.sid
+    # print [name for name,thing in inspect.getmembers(request)]
+    print 'remote connected ip: %s , %s'%(request.remote_addr,request.user_agent)
+
+@socketio.on('disconnect')
+def close():
+    print online
+    try:
+        uid = SidToUid[request.sid]
+        if uid in online: del online[uid]
+        if request.sid in SidToUid: del SidToUid[request.sid]
+        print online
+        for k,v in online.items():
+            emit('message',uid+" left",room=v)
+        print request.sid
+        print 'remote close ip: %s'%request.remote_addr
+    except Exception as e:
+          print e   
 
 @socketio.on('init')
 def handle_my_custom_event(json_data):
@@ -20,6 +41,11 @@ def handle_my_custom_event(json_data):
 def handle_my_custom_event(json_data):
     # print json_data["uid"]
     # print 'remote connected ip: %s'%request.remote_addr
+    uid = json_data["uid"]
+    if uid != "non-login" and uid not in online:
+        online[uid] = request.sid
+        SidToUid[request.sid] = uid
+
     json_data["remote_addr"] = request.remote_addr
     json_data["event"] = 'watch_interval'
     print json.dumps(json_data)
@@ -33,6 +59,11 @@ def handle_my_custom_event(json_data):
 def click_video(json_data):
     # print json_data["uid"]
     # print 'remote connected ip: %s'%request.remote_addr
+    uid = json_data["uid"]
+    if uid != "non-login" and uid not in online:
+        online[uid] = request.sid
+        SidToUid[request.sid] =uid
+
     json_data["remote_addr"] = request.remote_addr
     json_data["event"] = 'click_video'
     print json.dumps(json_data)
@@ -45,6 +76,11 @@ def click_video(json_data):
 def rec_list(json_data):
     # print json_data["uid"]
     # print 'remote connected ip: %s'%request.remote_addr
+    uid = json_data["uid"]
+    if uid != "non-login" and uid not in online:
+        online[uid] = request.sid
+        SidToUid[request.sid] =uid
+
     json_data["remote_addr"] = request.remote_addr
     json_data["event"] = 'rec_list'
     print json.dumps(json_data)
@@ -52,6 +88,11 @@ def rec_list(json_data):
 					topic="arn:aws:sns:us-east-1:612129620405:watch_interval",
 					message=json.dumps(json_data)
 	)
+
+@socketio.on('message')
+def handle_message(message):
+
+    print('received message: ' + message)
 	
 if __name__ == '__main__':
 
