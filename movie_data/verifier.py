9fdb241c4150337b422415bdcb41a98cffe05520
Mick Lin <cl3469@columbia.edu>
2016-12-15 19:49:54 -0500

    hash genre data

--- movie_data/verifier.py
+++ movie_data/verifier.py
@@ -1,4 +1,5 @@
 import csv,sys
+from collections import *
 
 
 movies = open('movies.csv', 'rb')
@@ -12,11 +13,19 @@ if len(sys.argv) !=2:
 	print "verifier.py movie_name"
 	sys.exit(1)
 # print collect
+genre_hash = defaultdict(lambda: [])
 
 ret ={}
 for row in mreader:
+	for x in row[2].split("|"):
+			genre_hash[x].append(row[0])
 	if sys.argv[1].lower() in row[1].lower():
+
 		print row
 
+out = open('genre.csv', 'wb') 
+spamwriter = csv.writer(out, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+for k, v in genre_hash.items():
+	spamwriter.writerow([k, v])
 movies.close()	
 
