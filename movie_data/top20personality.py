ab89a5669c2c553114fa212e995a209c2e1625d1
Mick Lin <cl3469@columbia.edu>
2016-12-15 23:38:36 -0500

    top20 done

---
+++ movie_data/top20personality.py
@@ -0,0 +1,18 @@
+import csv,sys,heapq
+from collections import *
+
+user_p = open('personality.csv', 'rb')
+person_reader = csv.reader(user_p, delimiter=',')
+h =[]
+for row in person_reader:
+	if len(h) < 20:
+		heapq.heappush(h, (len(row[1].split("|")),row[0]))
+	else:
+
+		heapq.heappushpop(h,  (len(row[1].split("|")),row[0]))
+	# if len(row[1].split("|")) > 1000:
+	# 	print row[0]
+
+for row in sorted(h, key= lambda x: -x[0]):
+	print "personality: %s \npopulation: %d\n"%(row[1],row[0])
+user_p.close()
