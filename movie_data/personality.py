20d1bd0364079cc16c04f311bfde03b7d20701ae
Mick Lin <cl3469@columbia.edu>
2016-12-16 00:46:38 -0500

    better profile

--- movie_data/personality.py
+++ movie_data/personality.py
@@ -14,32 +14,46 @@ for row in mreader:
 rreader = csv.reader(rate, delimiter=',')
 
 i=0
-user_personality = {}
+user_personality = defaultdict(lambda: defaultdict( lambda:0))
+rate_count = defaultdict(lambda: 0)
 for row in rreader:
 	if i ==0:
 		i+=1 
 		continue
-
+	rate_count[row[0]]+=1
 	if float(row[2]) >= 4.0:
 		# print genre_hash[row[1]]
 		s = set()
 		for gen in genre_hash[row[1]]:
+			user_personality[row[0]][gen]+=1 
 			s.add(gen)
-		if row[0] not in user_personality:
-			user_personality[row[0]] = s
-		else:
-			# print user_personality[row[0]]
-			user_personality[row[0]] = user_personality[row[0]].union(s)
+		# if row[0] not in user_personality:
+		# 	user_personality[row[0]] = s
+		# else:
+		# 	# print user_personality[row[0]]
+		# 	user_personality[row[0]] = user_personality[row[0]].union(s)
 		# print row 
 		i+=1
 
-	# if i==400: break
+	# if i==1000: break
 
+filter_personality =defaultdict(lambda: set())
+for uid, gen_list in user_personality.items():
+	i=0
+	s= set()
+	for gen, count in sorted(gen_list.items(), key= lambda x: -x[1]):
+		# print uid ,gen, count
+		if count >3 and i <7:
+			s.add(gen)
+			i+=1
+	filter_personality[uid] |= s
+# print rate_count
+# print filter_personality
 total_personality = defaultdict(lambda: [])
-for k, v in  user_personality.items():
-	sortl = sorted(list(v))
-	# print sortl
-	total_personality["|".join(sortl)].append(k)
+for k, v in  filter_personality.items():
+	if rate_count[k] >5:
+		sortl = sorted(list(v))
+		total_personality["|".join(sortl)].append(k)
 
 # print  total_personality
 out = open('personality.csv', 'wb') 
--- movie_data/top20personality.py
+++ movie_data/top20personality.py
@@ -5,13 +5,15 @@ user_p = open('personality.csv', 'rb')
 person_reader = csv.reader(user_p, delimiter=',')
 h =[]
 for row in person_reader:
+
+	if len(row[0]) ==0: continue
+
 	if len(h) < 20:
 		heapq.heappush(h, (len(row[1].split("|")),row[0]))
 	else:
 
 		heapq.heappushpop(h,  (len(row[1].split("|")),row[0]))
-	# if len(row[1].split("|")) > 1000:
-	# 	print row[0]
+
 
 for row in sorted(h, key= lambda x: -x[0]):
 	print "personality: %s \npopulation: %d\n"%(row[1],row[0])
