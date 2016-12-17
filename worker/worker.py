a989a8d17b8f28b4b9ed980ba5388122bec72ada
Mick Lin <cl3469@columbia.edu>
2016-12-17 16:42:03 -0500

    read all msg

--- worker/worker.py
+++ worker/worker.py
@@ -1,7 +1,8 @@
 import boto.sqs,boto.sns,json, inspect, threading, logging, time, requests
 conn = boto.sqs.connect_to_region("us-east-1",profile_name='movie')
 q=conn.get_queue('watch_interval')
-for message in q.get_messages():
-	body = json.loads(message.get_body())
-	msg =json.loads(body["Message"])
-	print body["Message"]
+while True:
+	for message in q.get_messages():
+		body = json.loads(message.get_body())
+		msg =json.loads(body["Message"])
+		print body["Message"]
